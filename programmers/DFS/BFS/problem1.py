2407260042d4f825a452e16d3f5c41fa31d6cbea
lucio <syy2585@naver.com>
2021-09-06 01:25:15 +0900

    update dfs/bfs problem

---
+++ programmers/DFS/BFS/problem1.py
@@ -0,0 +1,9 @@
+def solution(numbers, target):
+    answer = 0
+    return answer
+
+if __name__ == '__main__':
+    numbers = [1, 1, 1, 1, 1]
+    target = 3
+    s = solution(numbers, target)
+    print(s)
