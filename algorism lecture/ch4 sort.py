3347e029fc5ffde841d104a6deb83daea3d8b7cd
lucio <syy2585@naver.com>
2021-11-29 01:11:28 +0900

    update quick sort

--- algorism lecture/ch4 sort.py
+++ algorism lecture/ch4 sort.py
@@ -42,6 +42,57 @@ def insert_sort(arr):
     2. 기준 설정 후 다음 쿤 수와 작은 수 교환한 후 리스트 반으로 나누는 동작 방식
     3. 피벗: 교환하기 위한 기준
 '''
+def quick_sort(arr, start, end):
+    # 원소가 한 개인 경우 종료
+    if start >= end:
+        return
+    # 피벗은 첫 번째 원소
+    pivot = start
+    left = start + 1
+    right = end
+
+    while left <= right:
+        # 피벗보다 큰 데이터 찾을 때까지 반복
+        while left <= end and arr[left] <= arr[pivot]:
+            left += 1
+        # 피벗보다 작은 데이터 찾을 때까지 반복
+        while right > start and arr[right] >= arr[pivot]:
+            right -= 1
+        # 엇갈렸다면 작은 데이터와 피벗 교체
+        if left > right:
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        # 엇갈리지 않았다면 작은 데이터와 큰 데이터 교체
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    quick_sort(arr, start, right - 1)
+    quick_sort(arr, right + 1, end)
+
+'''
+    퀵 정렬 버전2
+'''
+def quick_sort2(arr):
+    # 리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(arr) <= 1:
+        return arr
+    
+    # 피벗은 첫 번째 원소
+    pivot = arr[0]
+    # 피벗을 제외한 리스트
+    tail = arr[1:]
+
+    # 분할된 왼쪽 부분
+    left_side = [x for x in tail if x <= pivot]
+    # 분할된 오른쪽 부분
+    right_side = [x for x in tail if x > pivot]
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행하고, 전체 리스트 반환
+    return quick_sort2(left_side) + [pivot] + quick_sort2(right_side)
 
 if __name__ == '__main__':
     test_arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+    # sorted_arr = quick_sort(test_arr, 0, len(test_arr) - 1)
+    # print(test_arr)
+
+    print(quick_sort2(test_arr))
