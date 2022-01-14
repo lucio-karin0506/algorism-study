16a8f38294b381f69a189c6ff7e52b37830203d0
lucio <syy2585@naver.com>
2022-01-14 13:11:38 +0900

    upload coloring prob

--- SW Expert Academy/Programming Intermediate/List2/1 coloring.py
+++ SW Expert Academy/Programming Intermediate/List2/1 coloring.py
@@ -4,18 +4,28 @@ res_list = []
 for test_case in range(1, T + 1):
     input_num = int(input())
 
-    # 모든 색칠 영역 리스트 담는 전체 리스트
-    all_num_list = []
+    # 10 * 10 도화지
+    canvas = [[0 for _ in range(10)] for _ in range(10)]
 
     # 입력한 숫자 크기만큼 숫자 위치 리스트 받음
-    for i in range(input_num):
-        num_list = list(map(int, input().split()))
-        all_num_list.append(num_list)
+    for _ in range(input_num):
+        start_x, start_y, end_x, end_y, color = map(int, input().split())
 
-    for num_list in all_num_list:
-        # red
-        if num_list[-1] == 1:
-            pass
-        # blue
-        elif num_list[-1] == 2:
-            pass
+        # 색칠 ㄱㄱ
+        for i in range(start_x, end_x+1):
+            for j in range(start_y, end_y+1):
+                canvas[i][j] += 1
+
+        # 겹치는 부분 탐색
+        count = 0
+        for row in range(len(canvas)):
+            # 행 리스트
+            canvas_row = canvas[row]
+            for col in range(len(canvas_row)):
+                if canvas[row][col] == 2:
+                    count += 1
+
+    res_list.append(count)
+
+for index, value in enumerate(res_list):
+    print(f'#{index+1} {value}')
