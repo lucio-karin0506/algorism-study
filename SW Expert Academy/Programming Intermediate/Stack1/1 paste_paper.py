b6bef75360278886d2a24e94ed86c0e2e90fc2d9
lucio <syy2585@naver.com>
2022-01-20 01:17:53 +0900

    update paste paper

--- SW Expert Academy/Programming Intermediate/Stack1/1 paste_paper.py
+++ SW Expert Academy/Programming Intermediate/Stack1/1 paste_paper.py
@@ -1,15 +1,19 @@
-def get_rect_num(size):
-    rect_num = ''
-    return rect_num
+def get_rect_num(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 3
+    # dynamic programmin, 점화식 형태
+    return get_rect_num(n-1) + 2*get_rect_num(n-2)
 
 if __name__ == '__main__':
     T = int(input())
     res = []
 
     for test_case in range(1, T + 1):
-        size = int(input())
-        rect_num = get_rect_num(size)
+        N = int(input())
+        rect_num = get_rect_num(N // 10)
         res.append(rect_num)
-
+        
     for idx, value in enumerate(res):
         print(f'#{idx+1} {value}')
