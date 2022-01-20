8ab6b67fb145faa48eeef7297fc6bacda09feae0
lucio-karin0506 <syy2585@naver.com>
2022-01-20 11:22:13 +0900

    update bracket check

--- SW Expert Academy/Programming Intermediate/Stack1/2 bracket_check.py
+++ SW Expert Academy/Programming Intermediate/Stack1/2 bracket_check.py
@@ -9,21 +9,29 @@ def check_bracket(input_str):
 
         # 끝 괄호면
         elif letter == '}' or letter == ')':
-            # 리스트에 원소 존재 시
-            if len(input_list) != 0:
-                pop_letter = input_list.pop()
             # 리스트에 원소 미 존재 시
-            else:
+            # 차피 끝 괄호가 먼저 들어갔기 때문에
+            # 짝 안맞음
+            if len(input_list) == 0:
+                input_list.append(letter)
                 is_bracket_check = 0
                 break
 
             # 각 괄호 형태가 서로 안맞고 다른 경우
-            if (pop_letter == '{' and letter == ')') or (pop_letter == '(' and letter == '}'):
+            elif (input_list[-1] == '{' and letter == ')') or (input_list[-1] == '(' and letter == '}'):
+                input_list.append(letter)
                 is_bracket_check = 0
                 break
 
+            # 리스트에 원소 존재 시
+            # 짝 맞는 경우
+            else:
+                input_list.pop()
+
+    # 리스트에 괄호 있으면
     if len(input_list) != 0:
         is_bracket_check = 0
+    # 리스트에 괄호 없으면
     else:
         is_bracket_check = 1
 
