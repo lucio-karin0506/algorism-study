53f8664fef8e03407edd87f3a419d07f09f147cd
lucio-karin0506 <syy2585@naver.com>
2022-01-21 17:59:28 +0900

    upload again

--- SW Expert Academy/Programming Intermediate/Stack2/1 forth.py
+++ SW Expert Academy/Programming Intermediate/Stack2/1 forth.py
@@ -1,11 +1,38 @@
 def forth(input_list):
     ans_list = []
+    ans = 0
 
     for letter in input_list:
-        if type(eval(letter)) != int:
-            pass
+        if type(int(letter)) == int:
+            ans_list.append(letter)
+        else:
+            if letter == '+':
+                ans = int(ans_list[0]) + int(ans_list[1])
+                ans_list.pop()
+                ans_list.pop()
+                ans_list.append(ans)
+            elif letter == '-':
+                ans = int(ans_list[0]) - int(ans_list[1])
+                ans_list.pop()
+                ans_list.pop()
+                ans_list.append(ans)
+            elif letter == '*':
+                ans = int(ans_list[0]) * int(ans_list[1])
+                ans_list.pop()
+                ans_list.pop()
+                ans_list.append(ans)
+            elif letter == '/':
+                ans = int(ans_list[0]) / int(ans_list[1])
+                ans_list.pop()
+                ans_list.pop()
+                ans_list.append(ans)
+            elif letter == '.':
+                if len(ans_list) == 1:
+                    ans = ans_list.pop()
+                else:                 
+                    return 'error'
 
-    return ans_list[0]
+    return ans
 
 if __name__ == '__main__':
     T = int(input())
