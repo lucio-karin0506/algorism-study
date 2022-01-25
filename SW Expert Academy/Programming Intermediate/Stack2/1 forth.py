7f7e66a90f4a4a8269f41c2a2f3c1f0772389580
lucio <syy2585@naver.com>
2022-01-25 13:06:33 +0900

    upload forth2 method

--- SW Expert Academy/Programming Intermediate/Stack2/1 forth.py
+++ SW Expert Academy/Programming Intermediate/Stack2/1 forth.py
@@ -31,6 +31,37 @@ def forth(input_list):
             elif letter == '/':
                 ans_list.append(n1 // n2)
 
+def forth2(input_list):
+    ans_list = []
+
+    for letter in input_list:
+        # 1. type check
+        if letter.isnumeric() == True:
+            ans_list.append(int(letter))
+        else:
+            if letter == '.':
+                if len(ans_list) == 1:
+                    ans = ans_list.pop()
+                    return ans
+                else:
+                    return 'error'
+
+            try:
+                num1 = ans_list.pop()
+                num2 = ans_list.pop()
+
+                if letter == '+':
+                    ans_list.append(num1+num2)
+                elif letter == '-':
+                    ans_list.append(num1-num2)
+                elif letter == '*':
+                    ans_list.append(num1*num2)
+                elif letter == '/':
+                    ans_list.append(num1//num2)
+
+            except IndexError:
+                return 'error'
+
 
 if __name__ == '__main__':
     T = int(input())
