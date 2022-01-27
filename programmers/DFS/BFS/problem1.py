97b261f1999ed5d4df3d1c917e67edcce624cd39
lucio-karin0506 <syy2585@naver.com>
2022-01-27 17:42:45 +0900

    upload prob1

--- programmers/DFS/BFS/problem1.py
+++ programmers/DFS/BFS/problem1.py
@@ -1,5 +1,20 @@
 def solution(numbers, target):
     answer = 0
+    visited = []
+
+    visited.append(0)
+
+    for num in numbers:
+        tmp = []
+        for i in visited:
+            tmp.append(i + num)
+            tmp.append(i - num)
+        visited = tmp
+
+    for i in visited:
+        if i == target:
+            answer += 1
+
     return answer
 
 if __name__ == '__main__':
