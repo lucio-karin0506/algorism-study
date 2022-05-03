2ff1e4ce914e0a615fbcd7153a76fe40dc3d43e8
lucio <syy2585@naver.com>
2022-05-03 15:01:51 +0900

    Modify: resolve

--- backjoon/dfs_bfs/S5_바닥장식.py
+++ backjoon/dfs_bfs/S5_바닥장식.py
@@ -1,30 +1,26 @@
-# redo
 def solution(graph, N, M):
     count = 0
 
-    # check -
-    for i in range(N):
-        pre = '/'
-        for j in range(M):
-            # - 일 경우 count 증가
-            if graph[i][j] == '-':
-                # 같은 모양일 경우, count 제외 ==> 이전 타일 모양과 다를 때만 확인 위함
-                if graph[i][j] != pre:
+    # 같은 행 나무 판자 판별
+    for a in range(N):
+        pos = '/'
+        for b in range(M):
+            if graph[a][b] == '-':
+                if graph[a][b] != pos:
                     count += 1
 
-            # 이전 타일 모양 정보 저장 => 다음 타일 모양과의 일치 여부 비교 위함
-            pre = graph[i][j]
+            pos = graph[a][b]
 
-    # check |
-    for j in range(M):
-        pre = '/'
-        for i in range(N):
-            # | 일 경우 count 증가
-            if graph[i][j] == '|':
-                if graph[i][j] != pre:
+    # 같은 열 나무 판자 판별
+    for b in range(M):
+        pos = '/'
+        for a in range(N):
+            # 카운트 세고 거쳤던 바닥문양 pos로 초기화
+            if graph[a][b] == '|':
+                if graph[a][b] != pos:
                     count += 1
 
-            pre = graph[i][j]
+            pos = graph[a][b]
 
     return count
 
