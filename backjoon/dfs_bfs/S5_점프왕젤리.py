bef57ca456ae57c8d5b1ef7289de9a2e67966d51
lucio <syy2585@naver.com>
2022-05-03 14:39:00 +0900

    Modify: resolve

--- backjoon/dfs_bfs/S5_점프왕젤리.py
+++ backjoon/dfs_bfs/S5_점프왕젤리.py
@@ -1,27 +1,26 @@
-# redo
-def solution(graph, start, visit):
-    queue = [start]
-    # down, right
-    dx = [1, 0]
-    dy = [0, 1]
+from collections import deque
 
-    # bfs
+def solution(graph, start, visit):
+    queue = deque()
+    queue.append(start)
+    visit[0][0] = 1
+
+    dx, dy = [1, 0], [0, 1]
+
     while queue:
         x, y = queue[0][0], queue[0][1]
         del queue[0]
 
-        # 맨 오른쪽 아래 도달하면 HaruHaru
+        # 맨 오른쪽 아래 도달 시
         if graph[x][y] == -1:
             return 'HaruHaru'
-        
-        # jump value
+
         jump = graph[x][y]
 
-        # 아래, 오른쪽 탐색
         for i in range(2):
             nx = x + dx[i] * jump
             ny = y + dy[i] * jump
-
+            
             # 범위 안에 있고, 미방문 시 방문 체크 후 queue에 삽입
             if 0 <= nx < len(graph) and 0 <= ny < len(graph) and visit[nx][ny] == 0:
                 visit[nx][ny] = 1
